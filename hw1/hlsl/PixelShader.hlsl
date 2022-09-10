3afcf0ee6d60b126f53898032959032b61d387f8
wuching <mr.wuching@gmail.com>
2022-09-10 21:37:14 +0800

    s

--- hw1/hlsl/PixelShader.hlsl
+++ hw1/hlsl/PixelShader.hlsl
@@ -1,10 +1,5 @@
-cbuffer color
-{
-	float4 face_corlor[6];
-};
 
 float4 main(float4 color : Color) : SV_TARGET
 {
-	return  color;
-	//return face_corlor[tid / 2];
+	return color;
 }
--- hw1/hlsl/VertexShader.hlsl
+++ hw1/hlsl/VertexShader.hlsl
@@ -12,12 +12,8 @@ cbuffer cBuf
 
 VsOut main(float2 pos : Position, float4 color : Color)
 {
-	VsOut vs;
-
-	vs.pos = float4(pos.x, pos.y, 0, 0);
-	vs.color = color;
-	return vs;
-	//return mul(ts, float4(pos.x, pos.y, pos.z, 1.0f));
-	
-	
+	VsOut vt;
+	vt.pos = mul(ts, float4(pos.x, pos.y, 0, 1.0f));
+	vt.color = color;
+	return vt;
 }
